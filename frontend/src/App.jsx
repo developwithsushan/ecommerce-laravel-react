10e7f55e2a28bd54e15d961d46e5c609ec20dc98
Sushan Paudyal <mailtosushan@gmail.com>
2025-08-25 11:58:21 +0545

    Cart Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/Home.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
+import Cart from "./components/Cart.jsx";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
                 <Route path='/' element={<Home/>} />
                 <Route path='/shop' element={<Shop/>} />
                 <Route path='/product' element={<Product/>} />
+                <Route path='/cart' element={<Cart/>} />
             </Routes>
         </BrowserRouter>
     </>
--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Logo from '../../assets/images/logo.png';
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
@@ -31,13 +32,13 @@ const Header = () => {
                                 </svg>
                             </a>
 
-                            <a href="" className='ms-3'>
+                            <Link to='/cart' className='ms-3'>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="28" fill="currentColor"
                                      className="bi bi-bag" viewBox="0 0 16 16">
                                     <path
                                         d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"></path>
                                 </svg>
-                            </a>
+                            </Link>
                         </div>
                     </Navbar.Collapse>
                 </Navbar>
