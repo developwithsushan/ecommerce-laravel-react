ba49fdcb3a7a93a63cb314d9dab9eab1d166050c
Sushan Paudyal <mailtosushan@gmail.com>
2025-08-25 14:39:37 +0545

    #3: Product, Cart & Checkout UI : Checkout Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 import Cart from "./components/Cart.jsx";
+import Checkout from "./components/Checkout.jsx";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
                 <Route path='/shop' element={<Shop/>} />
                 <Route path='/product' element={<Product/>} />
                 <Route path='/cart' element={<Cart/>} />
+                <Route path='/checkout' element={<Checkout/>} />
             </Routes>
         </BrowserRouter>
     </>
